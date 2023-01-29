0071734f07f390e391021f5dce6b22abc71569a2
lakshan190 <lvratnaike90@gmail.com>
2023-01-30 00:16:08 +0530

    new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -136,6 +136,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -144,6 +148,7 @@ const init = function () {
   searchViews.addHandleSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
